ff4127766cfb33ce676200d86733012b519a7687
Alec Wang <alec1993wang@hotmail.com>
2019-07-29 16:49:24 +1000

    Add click handler

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,1 +1,8 @@
-// Add your scripts here
+(() => {
+  function handleClick (event) {
+    event.preventDefault()
+    console.log(event.target)
+  }
+
+  document.querySelectorAll('a').forEach(value => value.addEventListener('click', handleClick))
+})()
